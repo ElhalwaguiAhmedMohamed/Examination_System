ae73c9215e370800ba7ab8c30017881aba9de594
ElhalwaguiAhmedMohamed <elhalwaguiahmedmohamed@gmail.com>
2022-02-21 20:50:19 +0200

    updating with exam router and controler

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -6,14 +6,38 @@ exports.loginController = function(req,res,next){
         const request = new sql.Request();
         request.input('name',sql.VarChar,req.body.name)
         request.execute('SelectStudentByName',(err,result)=>{
-        res.status(200).json({Data:result.recordsets});
+            if(result.recordset.length != 0){
+                if(req.body.password === result.recordsets[0][0].password){
+                    res.status(200).json({Data:result.recordsets});
+                }else{
+                    next(new Error("Wrong name or password"));
+                    res.status(404).json({Message:"Wrong name or password"});
+                }
+            }else{
+                next(new Error("Wrong name or password"));
+                res.status(404).json({Message:"Wrong name or password"});
+            }
+
         })
     }else if(req.body.user === 'Instructor'){
         const request = new sql.Request();
         request.input('name',sql.VarChar,req.body.name)
         request.execute('SelectInstructorByName',(err,result)=>{
-        res.status(200).json({Data:result.recordsets});
-        })
+            if(result.recordset.length != 0){
+                if(req.body.password === result.recordsets[0][0].password){
+                    res.status(200).json({Data:result.recordsets});
+                }else{
+                    next(new Error("Wrong name or password"));
+                    res.status(404).json({Message:"Wrong name or password"});
+                }
+
+            }else{
+                next(new Error("Wrong name or password"));
+                res.status(404).json({Message:"Wrong name or password"});
+            }
+
+        });
     }
 
 }
+
